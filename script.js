8931104a6629e011df6ce3a0b10708d1a7bdedad
hugo-lui <115033116+hugo-lui@users.noreply.github.com>
2022-12-22 17:01:30 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let currentYear = new Date().getFullYear();
+let birthYear = 2004;
+document.getElementById("currentAge").innerHTML = currentYear - birthYear;
+let typed = new Typed(".auto-type", {
+    strings: ["^500Hugo Lui^1000", "^500a Developer^1000"],
+    typeSpeed: 100,
+    backSpeed: 100
+})
+$(window).scroll(function() {
+    $('.fade').each(function() {
+        var top_of_element = $(this).offset().top;
+        var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+        var top_of_screen = $(window).scrollTop();
+
+        if((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element) && !$(this).hasClass('visible')) {
+            $(this).addClass('visible');
+        }
+        else if(((bottom_of_screen < top_of_element) || (top_of_screen > bottom_of_element)) && $(this).hasClass('visible')){
+            $(this).removeClass('visible');
+        }
+    });
+});
