ad75cae3399b9619872dbf0cbc30b9351e3fdec7
hugo-lui <115033116+hugo-lui@users.noreply.github.com>
2022-12-22 23:04:40 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ let typed = new Typed(".auto-type", {
 })
 $(window).scroll(function() {
     $('.fade').each(function() {
-        var top_of_element = $(this).offset().top;
-        var bottom_of_element = $(this).offset().top + $(this).outerHeight();
-        var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
-        var top_of_screen = $(window).scrollTop();
+        const top_of_element = $(this).offset().top;
+        const bottom_of_element = $(this).offset().top + $(this).outerHeight();
+        const bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+        const top_of_screen = $(window).scrollTop();
 
         if((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element) && !$(this).hasClass('visible')) {
             $(this).addClass('visible');
