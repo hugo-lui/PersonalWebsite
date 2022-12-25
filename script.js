e944fc4786a27dc374d5f3be78b343ba9355bf7e
hugo-lui <115033116+hugo-lui@users.noreply.github.com>
2022-12-25 11:33:24 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 let currentYear = new Date().getFullYear();
 let birthYear = 2004;
 document.getElementById("currentAge").innerHTML = currentYear - birthYear;
+
 let typed = new Typed(".auto-type", {
-    strings: ["^500Hugo Lui^1000", "^500a Developer^1000"],
+    strings: ["^500Hugo Lui^1000", "^500a Software Developer^1000"],
     typeSpeed: 100,
     backSpeed: 100
 })
+
 $(window).scroll(function() {
     $('.fade').each(function() {
-        const top_of_element = $(this).offset().top;
-        const bottom_of_element = $(this).offset().top + $(this).outerHeight();
-        const bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
-        const top_of_screen = $(window).scrollTop();
+        const topElement = $(this).offset().top;
+        const bottomElement = $(this).offset().top + $(this).outerHeight();
+        const bottomScreen = $(window).scrollTop() + $(window).innerHeight();
+        const topScreen = $(window).scrollTop();
 
-        if((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element) && !$(this).hasClass('visible')) {
+        if((bottomScreen > topElement) && (topScreen < bottomElement) && !$(this).hasClass('visible')) {
             $(this).addClass('visible');
         }
-        else if(((bottom_of_screen < top_of_element) || (top_of_screen > bottom_of_element)) && $(this).hasClass('visible')){
+        else if(((bottomScreen < topElement) || (topScreen > bottomElement)) && $(this).hasClass('visible')){
             $(this).removeClass('visible');
         }
     });
